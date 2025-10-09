fef6e17e8a98dbec2af300f8093cb6ad7d1925fb
JunhaoW3 <wupillow@gmail.com>
2025-10-10 00:26:02 +0800

    Remove unused imports from AddReminderCommandTest

--- src/test/java/seedu/address/logic/commands/AddReminderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddReminderCommandTest.java
@@ -12,11 +12,9 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
 import seedu.address.model.reminder.Reminder;
 
 public class AddReminderCommandTest {
