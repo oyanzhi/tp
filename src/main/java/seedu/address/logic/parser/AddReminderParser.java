dd80275886b0e38c542d1f9f03e74ff8276fa240
chloeongzy <chloeongzy05@gmail.com>
2025-10-09 17:50:24 +0800

    Add parser for add reminder

--- src/main/java/seedu/address/logic/parser/AddReminderParser.java
+++ src/main/java/seedu/address/logic/parser/AddReminderParser.java
@@ -1,4 +1,61 @@
 package seedu.address.logic.parser;
 
-public class AddReminderParser {
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_HEADER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Date;
+import java.util.stream.Stream;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
+
+public class AddReminderParser implements Parser<AddReminderCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddReminderCommand
+     * and returns an AddReminderCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddReminderCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_HEADER, PREFIX_DATE);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_HEADER)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddReminderCommand.MESSAGE_USAGE));
+        }
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddReminderCommand.MESSAGE_USAGE), pe);
+        }
+
+        String header = ParserUtil.parseHeader(argMultimap.getValue(PREFIX_HEADER).get());
+        Phone date = ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get());
+
+        Reminder reminder = new Reminder(index, header, date);
+
+        return new AddReminderCommand(reminder);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
 }
