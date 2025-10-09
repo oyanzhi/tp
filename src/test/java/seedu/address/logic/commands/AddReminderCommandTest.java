b8bb5a3d15a6066b2394d2347a1b7472a15c5900
JunhaoW3 <wupillow@gmail.com>
2025-10-10 00:44:29 +0800

    Add more test cases for AddReminderCommandTest

--- src/test/java/seedu/address/logic/commands/AddReminderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddReminderCommandTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +17,7 @@ import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
 import seedu.address.model.reminder.Reminder;
 
 public class AddReminderCommandTest {
@@ -27,12 +30,57 @@ public class AddReminderCommandTest {
     }
 
     @Test
+    public void execute_addReminder_success() {
+        Person personToEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Reminder reminder = new Reminder("Meet client on Monday", "2026-10-12T09:00");
+        AddReminderCommand command = new AddReminderCommand(INDEX_FIRST_PERSON, reminder);
+
+        Person editedPerson = personToEdit.addReminder(reminder);
+        String expectedMessage = String.format(AddReminderCommand.MESSAGE_ADD_REMINDER_SUCCESS,
+                editedPerson.getName(), reminder);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(personToEdit, editedPerson);
+
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+    }
+
+    @Test
     public void execute_invalidIndex_throwsCommandException() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
         Reminder reminder = new Reminder("Meet client on Monday", "2026-10-12T09:00");
         AddReminderCommand command = new AddReminderCommand(outOfBoundIndex, reminder);
 
         assertCommandFailure(command, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_sameReminderDifferentPersons_success() {
+        Reminder reminder = new Reminder("Team meeting", "2026-12-01T09:00");
+
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person secondPerson = model.getFilteredPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+
+        AddReminderCommand commandFirst = new AddReminderCommand(INDEX_FIRST_PERSON, reminder);
+        AddReminderCommand commandSecond = new AddReminderCommand(INDEX_SECOND_PERSON, reminder);
+
+        // Apply to first person
+        Person editedFirst = firstPerson.addReminder(reminder);
+        Model expectedModelFirst = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModelFirst.setPerson(firstPerson, editedFirst);
+
+        assertCommandSuccess(commandFirst, model,
+                String.format(AddReminderCommand.MESSAGE_ADD_REMINDER_SUCCESS, editedFirst.getName(), reminder),
+                expectedModelFirst);
+
+        // Apply to second person (should still succeed)
+        Person editedSecond = secondPerson.addReminder(reminder);
+        Model expectedModelSecond = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModelSecond.setPerson(secondPerson, editedSecond);
+
+        assertCommandSuccess(commandSecond, model,
+                String.format(AddReminderCommand.MESSAGE_ADD_REMINDER_SUCCESS, editedSecond.getName(), reminder),
+                expectedModelSecond);
     }
 
     @Test
