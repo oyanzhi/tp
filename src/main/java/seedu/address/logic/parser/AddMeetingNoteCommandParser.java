3c5225da5645e8dccc19dd91b29d90d015777dfc
chloeongzy <chloeongzy05@gmail.com>
2025-10-23 23:21:43 +0800

    Add logging for meeting notes

--- src/main/java/seedu/address/logic/parser/AddMeetingNoteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddMeetingNoteCommandParser.java
@@ -2,7 +2,12 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.ParserUtil.LOGGING_MESSAGE_PARSE_INDEX;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -15,6 +20,8 @@ import seedu.address.model.meetingnote.MeetingNote;
  * Parses input arguments and creates a new AddMeetingNoteCommand object
  */
 public class AddMeetingNoteCommandParser implements Parser<AddMeetingNoteCommand> {
+    private static final Logger logger = LogsCenter.getLogger(AddMeetingNoteCommandParser.class);
+
     /**
      * Parses the given {@code String} of arguments in the context of the AddMeetingNoteCommand
      * and returns an AddMeetingNoteCommand object for execution.
@@ -34,6 +41,7 @@ public class AddMeetingNoteCommandParser implements Parser<AddMeetingNoteCommand
 
         try {
             Index index = ParserUtil.parseIndex(splitArgs[0]);
+            logger.log(Level.INFO, LOGGING_MESSAGE_PARSE_INDEX + index.getOneBased());
             String note = ParserUtil.parseNote(splitArgs[1]);
             String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern(MeetingNote.DATE_PATTERN));
             MeetingNote meetingNote = new MeetingNote(note, date);
--- src/main/java/seedu/address/logic/parser/DeleteMeetingNoteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteMeetingNoteCommandParser.java
@@ -1,8 +1,14 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.ParserUtil.LOGGING_MESSAGE_PARSE_INDEX;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AddMeetingNoteCommand;
 import seedu.address.logic.commands.DeleteMeetingNoteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -10,6 +16,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
  * Parses input arguments and creates a new DeleteMeetingNoteCommand object
  */
 public class DeleteMeetingNoteCommandParser implements Parser<DeleteMeetingNoteCommand> {
+    private static final Logger logger = LogsCenter.getLogger(DeleteMeetingNoteCommandParser.class);
 
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteMeetingNoteCommand
@@ -19,6 +26,10 @@ public class DeleteMeetingNoteCommandParser implements Parser<DeleteMeetingNoteC
     public DeleteMeetingNoteCommand parse(String args) throws ParseException {
         try {
             Index[] indices = ParserUtil.parseDualIndex(args);
+            Index CLIENT_INDEX = indices[0];
+            Index MEETING_NOTE_INDEX = indices[1];
+            logger.log(Level.INFO, LOGGING_MESSAGE_PARSE_INDEX + CLIENT_INDEX.getOneBased());
+            logger.log(Level.INFO, LOGGING_MESSAGE_PARSE_INDEX + MEETING_NOTE_INDEX.getOneBased());
             return new DeleteMeetingNoteCommand(indices[0], indices[1]);
         } catch (ParseException pe) {
             throw new ParseException(
