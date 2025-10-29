85c651800b54e35ab2af7bc26119edb725f908a5
chloeongzy <chloeongzy05@gmail.com>
2025-10-29 21:52:27 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/parser/DeleteMeetingNoteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteMeetingNoteCommandParser.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.ParserUtil.LOGGING_MESSAGE_PARSE_INDEX;
 
 import java.util.logging.Level;
