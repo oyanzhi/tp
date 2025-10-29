6cfc6cf2bb3c89558f084209d7cb4caf82bf8a06
Asher <gohjinyuan@gmail.com>
2025-10-29 14:44:39 +0800

    Fix missing whitespace in add client display message, insurance policy validation, additional brackets in add client display message, incorrect parameter in add client display message

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -44,6 +44,8 @@ public class Messages {
                 .append(person.getEmail())
                 .append("; Address: ")
                 .append(person.getAddress())
+                .append("; Insurance Policy: ")
+                .append(person.getPolicy())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
         return builder.toString();
